371f1e7d69307f0c28db19602fbe8ad22f256a4c
Tang Rou <99491612+Qual1dea@users.noreply.github.com>
2023-06-05 01:46:02 +0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Money = new string[] { "Рубль", "Тенге", "Доллар" };
+            ViewBag.Cities = new string[] { "Ульяновск", "Казань", "Москва" };
+            string message = "Что за текст? <input>";
+            return View((object)message);
+            //return Viev(); 
+        }
+     
+        [HttpGet]
+        public ViewResult Form() => View();
+
+        [HttpPost]
+        public ViewResult Form(HomeModels x)
+        {
+            if (ModelState.IsValid) 
+            {
+                return View("Last", x);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
+
---
+++ Models/HomeModels.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class HomeModels
+    {
+        //[HiddenInput(DisplayValue = false)]
+        [Display(Name = "Имя")]
+        [UIHint("MultilineText")]
+        [Required(ErrorMessage ="Введите Имя")] 
+        public string? FirstName {get;set;}
+
+        [Display(Name = " Фамилия")]
+        [UIHint("MultilineText")]
+        [Required(ErrorMessage = "Введите Фамилию")]
+        public string? LastName { get;set;}
+        
+        [Display(Name = "Дата Рождения")]
+        [DataType(DataType.Date)]
+        [Required (ErrorMessage = "Введите Дату Рождения")]
+        public DateTime BirthDate { get;set;}
+
+        [Display(Name = "Телефон")]
+        [StringLength(11, MinimumLength = 10, ErrorMessage = "Длина строки должна быть от 10, до 11 символов")]
+        [Required(ErrorMessage = "Введите Телефон")]
+        public string? Phone { get;set;}
+
+        [Display(Name = "Год")]
+        [Range(1700, 2000, ErrorMessage = "Недопустимый год")]
+        public int Year { get; set; }
+
+        public bool? Will { get; set; }
+
+    }
+}
